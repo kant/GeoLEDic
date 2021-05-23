7e8603c1e71f01dfc934972d301e234cdc0b2efd
Samuel Nobs <3915450+samsta@users.noreply.github.com>
2021-05-23 20:49:02 +1200

    Switch to OctoWS library
    Use OctoWS to write to the LED using DMA, but use FastLEDs color
    utilities. Turns out that we can just pass in the CRGB array as the
    drawing memory so we don't even need FastLEDs OctoWS controller!
    
    Wow... just wow. The speed gains are impressive.
    
    The CPU load for the 1200 LEDs with FastLED was around 90%.
    Now, thanks to OctoWS, it's 3.4%... insane!

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Arduino.h"
+#include <OctoWS2811.h>
 #include "FastLED.h"
 #include "GeoLEDic.hpp"
 
@@ -6,21 +7,32 @@
 #define COLOR_ORDER RGB
 #define CHIPSET     WS2813
 #define NUM_LEDS    1200
+#define NUM_STRIPS  1
 
-#define NUM_STRIPS 1
 #define FRAME_RATE 24
-
 static const unsigned FRAME_INTERVAL_MS = 1000 / FRAME_RATE;
+
+static const int LOADMON_PIN = 14;
+
+DMAMEM int dma_mem[NUM_LEDS * 3 * (NUM_STRIPS + 3)/4];
+static const uint8_t pinlist[NUM_STRIPS] = {0};
+
+OctoWS2811 leds(NUM_LEDS, dma_mem, colors, WS2811_RGB | WS2813_800kHz, NUM_STRIPS, pinlist);
+
 
 #ifdef USB_MIDI_SERIAL    
 class LoadMonitor
 {
 public:
-    LoadMonitor(): m_accumulated_time_ms(0), m_count(0)
-    {}
-
-    void update(unsigned time_ms)
+    LoadMonitor(unsigned pin): m_accumulated_time_ms(0), m_count(0), m_pin(pin)
     {
+        pinMode(m_pin, OUTPUT);
+    }
+
+    void end(unsigned time_ms)
+    {
+#ifdef USB_MIDI_SERIAL
+
         m_accumulated_time_ms += time_ms;
         m_count++;
         if (m_count == FRAME_RATE * 2) // print every 2 seconds
@@ -29,13 +41,23 @@ public:
             m_accumulated_time_ms = 0;
             m_count = 0;
         }
+#else
+        (void)time_ms;
+#endif
+        digitalWrite(m_pin, LOW);
+    }
+
+    void start()
+    {
+        digitalWrite(m_pin, HIGH);
     }
 
     unsigned m_accumulated_time_ms;
     unsigned m_count;
+    unsigned m_pin;
 };
 
-static LoadMonitor load_monitor;
+static LoadMonitor load_monitor(LOADMON_PIN);
 #endif
 
 
@@ -44,24 +66,19 @@ void setup() {
     Serial.begin(115200); 
 #endif   
     delay(3000); // sanity delay
-    FastLED.addLeds<NUM_STRIPS, CHIPSET, LED_PIN, COLOR_ORDER>(colors, NUM_LEDS).setCorrection(TypicalLEDStrip);
-    FastLED.setBrightness(50);
 
     setupGeoLEDic();
+    leds.begin();
 }
 
 void loop()
 {
     uint32_t time_before = millis();
+    load_monitor.start();
     loopGeoLEDic();
-    FastLED.show(); 
+    leds.show();
 
     uint32_t dt = millis() - time_before;
-#ifdef USB_MIDI_SERIAL
-    load_monitor.update(dt);
-#endif
-    if (dt < FRAME_INTERVAL_MS)
-    {
-        FastLED.delay(FRAME_INTERVAL_MS - dt);
-    }
+    load_monitor.end(dt);
+    delay(FRAME_INTERVAL_MS - dt);
 }
