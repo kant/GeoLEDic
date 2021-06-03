81d053256cdc982b6d9a310f872d2504b0ba203a
Samuel Nobs <3915450+samsta@users.noreply.github.com>
2021-06-03 19:23:17 +1200

    Slightly more regular strip layout
    This is a bit less messy, particularly around the apex of the dome
    where wiring is the most painful

--- src/Dome.hpp
+++ src/Dome.hpp
@@ -6,7 +6,7 @@
 
 #define DOME_NUM_TRIANGLES 123
 #define NUM_STRIPS 32
-#define LEDS_PER_STRIP 1260
+#define LEDS_PER_STRIP 1265
 
 extern CRGB strips[NUM_STRIPS * LEDS_PER_STRIP];
 extern Triangle dome[DOME_NUM_TRIANGLES];
