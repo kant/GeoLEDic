e95b18be4d2c2b28b72e5986094fe7534425b30d
Samuel Nobs <3915450+samsta@users.noreply.github.com>
2021-06-06 18:54:14 +1200

    Break SerialFromKeyboard out into its own file

--- src/main-pc.cpp
+++ src/main-pc.cpp
@@ -4,39 +4,6 @@
 #include <unistd.h>
 #include "Serial.hpp"
 
-void SerialFromKeyboard::handleKey(char c)
-{
-   m_input.push(c);
-}
-
-bool SerialFromKeyboard::available()
-{
-   return not m_input.empty();
-}
-
-char SerialFromKeyboard::read()
-{
-   char c = ' ';
-   if (available())
-   {
-      c = m_input.front();
-      m_input.pop();
-   }
-   return c;
-}
-
-void SerialFromKeyboard::println(const char* msg)
-{
-   printf("%s\n", msg);
-}
-void SerialFromKeyboard::print(char c)
-{
-   printf("%c", c);
-}
-
-
-SerialFromKeyboard Serial;
-
 int main()
 {
 
