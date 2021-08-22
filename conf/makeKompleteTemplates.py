4fd8bf2020e0a4793f0fca50eb7a9966c9254a0b
Sam Nobs <3915450+samsta@users.noreply.github.com>
2021-08-22 16:32:57 +1200

    Autogenerate and install templates for Komplete Kontrol
    Template definition for programs and their controls so they get used
    to label the knobs and button on the Komplete Kontrol.
    
    I am not impressed with the functionality provided by Komplete Kontrol:
    
    - I have to install the file behind Komplete Kontrol's back as it doesn't
      offer a file loading mechanism
    - Komplete Kontrol doesn't expose all the functionality that's available. If
      we want a better UI (e.g. display images and more text) we have to use the
      HID interface like e.g. https://github.com/GoaSkin/qKontrol. This would allow
      to come up with a better solution for the enum parameters (e.g. pop-up list
      above the knobs)
    - Komplete Kontrol does not work stand-alone, it always needs a computer to
      load its brains. This is unsatisfactory because I was really hoping for
      a stand alone controller that could be plugged into the Teensy directly
      in the future, once the USB host is implemented. Unless, of course, the
      Teensy also takes over the HID interface...

---
+++ conf/makeKompleteTemplates.py
@@ -0,0 +1,264 @@
+#!/usr/bin/python
+
+import programs
+
+INDENT_STEP = "    "
+
+MIDI_TYPES = {
+    "control_change": 3,
+    "program_change": 4
+}
+
+BEHAVIOUR = {
+    "toggle": 0,
+    "trigger": 1
+}
+
+def writeKeyZone(indent, keyzone):
+    channel = (keyzone['channel'] if 'channel' in keyzone else 0)
+    # colors are arranged in a rainbow, but we want higher contrast
+    # between adjacent colors
+    color = (channel * 5) % 16 + 1
+
+    print indent, '"UpperKey": "%d",' % (keyzone['to'] if 'to' in keyzone else 127)
+    print indent, '"Transpose": "%d",' % (keyzone['transpose'] if 'transpose' in keyzone else 0)
+    print indent, '"Channel": "%d",' % channel
+    print indent, '"Off": "%s",' % (keyzone['off'] if 'off' in keyzone else 'false')
+    print indent, '"Color": "%d"' % color
+
+
+def writeKeyZones(indent, keyzones):
+    # disable keys in the gaps between zones
+    i = 0
+    last_to = 0
+    while i < len(keyzones):
+        if (keyzones[i]['from'] > last_to + 1):
+            keyzones.insert(i, {'off': 'true', 'from': last_to + 1, 'to': keyzones[i]['from'] - 1})
+        last_to = keyzones[i]['to']
+        i = i+1
+    # disable remaining keys if we're not using all of them
+
+    if last_to != 127:
+        keyzones.append({'off': 'true', 'to': 127})
+
+    for i in xrange(0, len(keyzones)):
+        print indent, '"%d": {' % i
+
+        writeKeyZone(indent, keyzones[i])
+
+        if i < len(keyzones) - 1:
+            print indent, '},'
+        else:
+            print indent, '}'
+
+
+def writeKnob(indent, knob):
+    print indent, '"Name": "%s",' % (knob['name'] if 'name' in knob else '')
+    print indent, '"MIDIType": "3",'
+    print indent, '"MIDIId": "%d",' % (knob['number'] if 'number' in knob else 14)
+    print indent, '"Channel": "0",'
+    print indent, '"Min": "0",'
+    print indent, '"Max": "127",'
+    print indent, '"Inc": "1",'
+    print indent, '"Mode": "0"'
+
+def writeKnobs(indent, knobs):
+    for i in xrange(0, 8):
+        print indent, '"%d": {' % i
+        if i < len(knobs):
+            writeKnob(indent + INDENT_STEP, knobs[i])
+        else:
+            # write disabled knob
+            writeKnob(indent + INDENT_STEP, {})
+        if i < 7:    
+            print indent, '},'
+        else:    
+            print indent, '}'
+
+def writeButton(indent, button):
+    print indent, '"Name": "%s",' % (button['name'] if 'name' in button else '')
+    print indent, '"MIDIType": "%d",' %(button['midi_type'] if 'midi_type' in button else 3)
+    print indent, '"MIDIId": "%d",' %(button['number'] if 'number' in button else 14)
+    print indent, '"Channel": "0",'
+    print indent, '"Off": "0",'
+    print indent, '"On": "%s",' % (button['on'] if 'on' in button else 127)
+    print indent, '"Inc": "0",'
+    print indent, '"Behavior": "%d"' % (button['behaviour'] if 'on' in button else 0)
+
+def writeButtons(indent, buttons):
+    for i in xrange(0, 8):
+        print indent, '"%d": {' % i
+        if i < len(buttons):
+            writeButton(indent + INDENT_STEP, buttons[i])
+        else:
+            # write disabled button
+            writeButton(indent + INDENT_STEP, {})
+        if i < 7:    
+            print indent, '},'
+        else:    
+            print indent, '}'
+
+def addProgramChangeButton(buttons, program_number):
+    buttons.append({
+        'name': 'Select Prog',
+        'midi_type': MIDI_TYPES['program_change'],
+        'behaviour': BEHAVIOUR['trigger'],
+        'on': program_number
+    })
+
+def addControls(buttons, knobs, controls):
+    for cc in controls:
+        if cc['type'] == 'toggle':
+            cc['behaviour'] = BEHAVIOUR['toggle']
+            buttons.append(cc)
+        elif cc['type'] == 'enum':
+            for i in xrange(0, len(cc['enums'])):
+                buttons.append({
+                    'name': "%s:%s" % (cc['short_name'] if 'short_name' in cc else cc['name'], cc['enums'][i]),
+                    'behaviour': BEHAVIOUR['trigger'],
+                    'number': cc['number'],
+                    'on': cc['values'][i]
+                })
+        else:
+            knobs.append(cc)
+
+def writeTemplate(indent, template, template_index):
+    print indent, '"Name": "%s",' % template['name']
+    zoneindent = indent + INDENT_STEP
+    pageindent = indent + INDENT_STEP
+    knobindent = pageindent + INDENT_STEP
+    
+    print indent, '"keyzones": {'
+    writeKeyZones(zoneindent, template['keyzones'])
+    print indent, '},'
+
+    buttons = []
+    knobs =[]
+    addProgramChangeButton(buttons, template_index)
+    addControls(buttons, knobs, template['controls'])
+
+    # we need at least one page, even if all buttons are unused on it
+    #  or else the Komplete Kontrol app crashes
+    num_pages = max(1, (max(len(buttons), len(knobs)) + 7)/8)
+
+    print indent, '"pages": {'
+    for i in xrange(0, num_pages):
+        print pageindent, '"%d": {' % i
+
+        print knobindent, '"knobs": {'
+        writeKnobs(knobindent + INDENT_STEP, knobs)
+        print knobindent, '},'
+
+        print knobindent, '"buttons": {'
+        writeButtons(knobindent + INDENT_STEP, buttons)
+        print knobindent, '}'
+
+        knobs = knobs[8:]
+        buttons = buttons[8:]
+        
+        if i < num_pages - 1:
+            print pageindent, '},'
+        else:
+            print pageindent, '}'
+
+    print indent, '}'
+
+def writeTemplates(indent, progs):
+    print indent, '"templates": {'
+    newindent = indent + INDENT_STEP
+    for i in xrange(0, len(progs)):
+        print newindent, '"%d": {' % i
+        writeTemplate(newindent + INDENT_STEP, progs[i], i)
+        if i < len(progs) - 1:
+            print newindent, "},"
+        else:
+            print newindent, "}"
+    print indent, "},"
+
+def writeDatFile(progs):
+    print "{"
+    indent = INDENT_STEP
+    print indent, '"Version": "4",'
+    print indent, '"ActiveTemplateIndex": "0",'
+    print indent, '"VelocityCurve": "3",'
+    writeTemplates(indent, progs)
+    print '    "pedals": { \n\
+        "pedalA": {\n\
+            "MIDIType": "3",\n\
+            "MIDIId": "11",\n\
+            "Channel": "0",\n\
+            "Min": "0",\n\
+            "Max": "127"\n\
+        },\n\
+        "pedalConfigA": {\n\
+            "ContiniuousMode": "false",\n\
+            "Invert": "false",\n\
+            "SwapTR": "false"\n\
+        },\n\
+        "fsTipA": {\n\
+            "MIDIType": "3",\n\
+            "MIDIId": "64",\n\
+            "Channel": "0",\n\
+            "Off": "0",\n\
+            "On": "127",\n\
+            "Inc": "1",\n\
+            "Behavior": "2"\n\
+        },\n\
+        "fsRingA": {\n\
+            "MIDIType": "3",\n\
+            "MIDIId": "65",\n\
+            "Channel": "0",\n\
+            "Off": "0",\n\
+            "On": "127",\n\
+            "Inc": "1",\n\
+            "Behavior": "2"\n\
+        },\n\
+        "pedalB": {\n\
+            "MIDIType": "3",\n\
+            "MIDIId": "11",\n\
+            "Channel": "0",\n\
+            "Min": "0",\n\
+            "Max": "127"\n\
+        },\n\
+        "pedalConfigB": {\n\
+            "ContiniuousMode": "true",\n\
+            "Invert": "false",\n\
+            "SwapTR": "false"\n\
+        },\n\
+        "fsTipB": {\n\
+            "MIDIType": "3",\n\
+            "MIDIId": "64",\n\
+            "Channel": "0",\n\
+            "Off": "0",\n\
+            "On": "127",\n\
+            "Inc": "1",\n\
+            "Behavior": "2"\n\
+        },\n\
+        "fsRingB": {\n\
+            "MIDIType": "3",\n\
+            "MIDIId": "65",\n\
+            "Channel": "0",\n\
+            "Off": "0",\n\
+            "On": "127",\n\
+            "Inc": "1",\n\
+            "Behavior": "2"\n\
+        }\n\
+    },\n\
+    "touchstrip": {\n\
+        "MIDIType": "3",\n\
+        "MIDIId": "11",\n\
+        "Channel": "0",\n\
+        "Min": "0",\n\
+        "Max": "127",\n\
+        "Param1": "4",\n\
+        "CenterValue": "0",\n\
+        "PhysicsModel": "0"\n\
+    }'
+    print "}"
+
+
+writeDatFile(programs.get())
+
+#for i in xrange(0, len(programs)):
+#    print programs[i]['program']
+
---
+++ conf/programs.py
@@ -0,0 +1,178 @@
+programs = [
+    {
+        'program':'DefaultRainbow',
+        'name':'Default Rainbow',
+        'controls': [
+            {
+                'name': 'Speed',
+                'number': 16,
+            },
+            {
+                'name': 'Stride',
+                'number': 17,
+            },
+            {
+                'name': 'Sparkle Probability',
+                'number': 19,
+            }
+        ]
+    },
+    {
+        'program':'SparklesAndTriangles',
+        'name':'Sparkles and Triangles',
+        'keyzones' : [
+            {
+                'name': 'Pentagon 0',
+                'from': 12,
+                'to': 17,
+                'channel': 1,
+                'transpose': 24
+            },
+            {
+                'name': 'Pentagon 1',
+                'from': 24,
+                'to': 33,
+                'channel': 1,
+                'transpose': 24
+            },
+            {
+                'name': 'Pentagon 2',
+                'from': 36,
+                'to': 42,
+                'channel': 1,
+                'transpose': 24
+            },
+            {
+                'name': 'Pentagon 3',
+                'from': 48,
+                'to': 57,
+                'channel': 1,
+                'transpose': 24
+            },
+            {
+                'name': 'blobs',
+                'from': 60,
+                'to': 111,
+                'channel': 2,
+                'transpose': -24
+            }
+
+        ],
+        'controls': [
+            {
+                'name': 'Red Worm',
+                'number': 16,
+            },
+            {
+                'name': 'Green Worm',
+                'number': 17,
+            },
+            {
+                'name': 'Blue Worm',
+                'number': 18,
+            },
+            {
+                'name': 'Sparkle Probability',
+                'number': 19,
+            }
+        ]
+    },
+    {
+        'program':'Fire',
+        'controls': [
+            {
+                'name': 'Cooling',
+                'number': 16
+            },
+            {
+                'name': 'Sparking',
+                'number': 17
+            },
+            {
+                'name': 'Downwards',
+                'number': 19,
+                'type': 'toggle'
+            },
+            {
+                'name': 'Reverse Palette',
+                'number': 20,
+                'type': 'toggle'
+            },
+            {
+                'name': 'Palette',
+                'short_name': 'Pal',
+                'type': 'enum',
+                'number': 18,
+                'enums': [
+                    'Fire',
+                    'Ocean',
+                    'Cloud',
+                    'Forest',
+                    'Lava',
+                    'Rainbow',
+                    'Party'
+                ]
+            }
+        ]
+
+    },
+    {
+        'program':'MovingRainbow',
+        'name':'Moving Rainbow',
+        'controls': [
+            {
+                'name': 'Speed Vertical',
+                'number': 16,
+            },
+            {
+                'name': 'Stride Vertical',
+                'number': 17,
+            },
+            {
+                'name': 'Speed Horizontal',
+                'number': 18,
+            },
+            {
+                'name': 'Stride Horizontal',
+                'number': 19,
+            }
+        ]
+    },
+    {
+        'program':'Lightning',
+        'controls': [
+            {
+                'name': 'Lightning Probability',
+                'number': 16,
+            },
+            {
+                'name': 'Forking Probability',
+                'number': 17,
+            }
+        ]
+    }
+]
+
+
+
+def get():    
+    # fill in defaults and expand values where needed (e.g. enums)
+    for i in xrange(0, len(programs)):
+
+        if not 'name' in programs[i]:
+            programs[i]['name'] = programs[i]['program']
+
+        if not 'keyzones' in programs[i]:
+            programs[i]['keyzones'] = []
+
+        if not 'controls' in programs[i]:
+            programs[i]['controls'] = []
+        else:
+            for cc in programs[i]['controls']:
+                if not 'type' in cc:
+                    cc['type'] = 'continuous'
+                if cc['type'] == 'enum':
+                    # spread enum values on range 0..127 to make it easier to use them with a knob
+                    n = len(cc['enums'])
+                    cc['values'] = range(0, n*(127/n), 127/n)
+    return programs
